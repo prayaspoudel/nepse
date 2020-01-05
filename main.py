3f746e4de0c624adaaffaf29958a5b052644db70
Daze <dazehere@yandex.com>
2020-01-05 23:20:51 +0545

    Removed unnecessary stuffs and created scraper to get company list.

--- main.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-company_list_link = "http://www.nepalstock.com/company/index/1/?stock-name=&stock-symbol=&sector-id=&_limit=500"
-
-company_get_request = requests.get(company_list_link)
-
-companyListSoup = company_get_request.content
